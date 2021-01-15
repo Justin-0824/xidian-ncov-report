f353dc7d9c7cd9ed2c57d985413fd7dee6e76a61
Justin-0824 <61155389+Justin-0824@users.noreply.github.com>
2021-01-15 10:34:30 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -36,11 +36,11 @@ print("#####\n温馨提示： 不外出、不聚集、不吃野味， 戴口罩�
 data = {}
 
 # 统一认证账号密码
-data.update({"_u":AskText("统一认证账号: ")})
-data.update({"_p":AskText("统一认证密码: ")})
+data.update({"_u":AskText("统一认证账号:19090100008 ")})
+data.update({"_p":AskText("统一认证密码:just010406 ")})
 
 # 今日是否在校
-data.update({"sfzx":AskBoolean("今日是否在校: "),"fxyy":""})
+data.update({"sfzx":AskBoolean("今日是否在校: 否"),"fxyy":""})
 
 # 返校原因
 if data["sfzx"] == 1:
@@ -59,28 +59,28 @@ else:
         data["city"] = data["province"]
     
 # 当前地点与上次不在同一城市
-data.update({"ismoved":AskBoolean("当前地点与上次是否不在同一城市: "),"bztcyy":""})
+data.update({"ismoved":AskBoolean("当前地点与上次是否不在同一城市:否 "),"bztcyy":""})
 if data["ismoved"] == 1:
     data.update({"bztcyy":AskInteractive("当前地点与上次不在同一城市,原因如下: ",["其他","探亲","旅游","回家"])})
 
 # 今日是否在中高风险地区
-data.update({"zgfxdq":AskBoolean("今日是否在中高风险地区：")})
+data.update({"zgfxdq":AskBoolean("今日是否在中高风险地区：否")})
 
 # 体温范围
-data.update({"tw":AskInteractive("今日体温范围(℃): ",["(-inf, 36]","(36, 36.5]","(36.5, 36.9]","(36.9, 37.3]","(37.3, 38]","(38, 38.5]","(38.5, 39]","(39, 40]","(40, +inf)"],1)})
+data.update({"tw":AskInteractive("今日体温范围(℃):(36.5,36.9] ",["(-inf, 36]","(36, 36.5]","(36.5, 36.9]","(36.9, 37.3]","(37.3, 38]","(38, 38.5]","(38.5, 39]","(39, 40]","(40, +inf)"],1)})
 
 # 今日是否到过或者经停武汉
-#data.update({"sftjwh":AskBoolean("今日是否到过或者经停武汉: ")})
+#data.update({"sftjwh":AskBoolean("今日是否到过或者经停武汉: 否")})
 
 # 今日是否到过或者经停湖北其他地区(除武汉)
-#data.update({"sftjhb":AskBoolean("今日是否到过或者经停湖北其他地区(除武汉): ")})
+#data.update({"sftjhb":AskBoolean("今日是否到过或者经停湖北其他地区(除武汉):否 ")})
 
 # 今日是否出现发热、乏力、干咳、呼吸困难等症状
-data.update({"sfcxtz":AskBoolean("今日是否出现发热、乏力、干咳、呼吸困难等症状: "),"sfyyjc":0,"jcjgqr":0,"jcjg":""})
+data.update({"sfcxtz":AskBoolean("今日是否出现发热、乏力、干咳、呼吸困难等症状: 否"),"sfyyjc":0,"jcjgqr":0,"jcjg":""})
 
 # 是否到相关医院或门诊检查
 if data["sfcxtz"] == 1:
-    data.update({"sfyyjc":AskBoolean("是否到相关医院或门诊检查: ")})
+    data.update({"sfyyjc":AskBoolean("是否到相关医院或门诊检查: 否")})
     
 # 检查结果属于以下哪种情况
 if data["sfyyjc"] == 1:
@@ -91,13 +91,13 @@ if data["jcjgqr"] in [1, 2, 3]:
     data.update({"jcjg":AskText("诊疗情况: ")})
     
 # 今日是否与武汉市或武汉周边的人员有过较为密集的接触
-#data.update({"sfjcwhry":AskBoolean("今日是否与武汉市或武汉周边的人员有过较为密集的接触: ")})
+#data.update({"sfjcwhry":AskBoolean("今日是否与武汉市或武汉周边的人员有过较为密集的接触:否 ")})
 
 # 今日是否与湖北其他地区(除武汉外)的人员有过较为密集的接触
-#data.update({"sfjchbry":AskBoolean("今日是否与湖北其他地区(除武汉外)的人员有过较为密集的接触: ")})
+#data.update({"sfjchbry":AskBoolean("今日是否与湖北其他地区(除武汉外)的人员有过较为密集的接触:否 ")})
 
 # 今日是否接触无症状感染/疑似/确诊人群
-data.update({"sfjcbh":AskBoolean("今日是否接触无症状感染/疑似/确诊人群: "),"jcbhlx":"","jcbhrq":""})
+data.update({"sfjcbh":AskBoolean("今日是否接触无症状感染/疑似/确诊人群: 否"),"jcbhlx":"","jcbhrq":""})
 
 if data["sfjcbh"] == 1:
     # 接触人群类型
@@ -107,16 +107,16 @@ if data["sfjcbh"] == 1:
     data.update({"jcbhrq":AskText("接触日期(YYYY-MM-DD): ")})
 
 # 今日是否接触密接人员
-data.update({"mjry":AskBoolean("今日是否接触密切接触人员: ")})
+data.update({"mjry":AskBoolean("今日是否接触密切接触人员:否 ")})
 
 # 近14日内本人/共同居住者是否去过疫情发生场所（市场、单位、小区等）或与场所人员有过密切接触
-data.update({"csmjry":AskBoolean("近14日内本人/共同居住者是否去过疫情发生场所（市场、单位、小区等）或与场所人员有过密切接触: ")})
+data.update({"csmjry":AskBoolean("近14日内本人/共同居住者是否去过疫情发生场所（市场、单位、小区等）或与场所人员有过密切接触:否 ")})
 
 # 今日是否接触境外人员
-data.update({"sfjcjwry":AskBoolean("今日是否接触境外人员: ")})
+data.update({"sfjcjwry":AskBoolean("今日是否接触境外人员: 否")})
 
 # 是否处于隔离期
-data.update({"sfcyglq":AskBoolean("是否处于隔离期: "),"gllx":"","glksrq":""})
+data.update({"sfcyglq":AskBoolean("是否处于隔离期: 否"),"gllx":"","glksrq":""})
 
 
 if data["sfcyglq"] == 1:
@@ -127,7 +127,7 @@ if data["sfcyglq"] == 1:
     data.update({"glksrq":AskText("隔离开始日期(YYYY-MM-DD): ")})
     
 # 是否有任何与疫情相关的,值得注意的情况
-data.update({"sfcxzysx":AskBoolean("是否有任何与疫情相关的,值得注意的情况: "),"qksm":""})
+data.update({"sfcxzysx":AskBoolean("是否有任何与疫情相关的,值得注意的情况: 否"),"qksm":""})
 
 # 情况说明
 if data["sfcxzysx"] == 1:
